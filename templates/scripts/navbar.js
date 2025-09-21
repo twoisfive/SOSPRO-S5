4423518f1e9419a482ef3c900fea7fa10e0f8916
RedStone576 <redgaming576@gmail.com>
2025-09-21 15:11:36 +0700

    wip: main page and nav code

---
+++ templates/scripts/navbar.js
@@ -0,0 +1,33 @@
+const $ = (id) => document.getElementById(id)
+
+switch (window.location.pathname.split("/")[1] ?? "")
+{
+    case "": {
+        $("navbar-header").classList.add("header-bg-custom1")
+        $("nav-item-button1").classList = "nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill"
+
+        break
+    }
+
+    case "about-us": {
+        $("navbar-header").classList.add("header-bg-custom2")
+        $("nav-item-button2").classList = "nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill"
+        
+        break
+    }
+
+    case "event": {
+        $("navbar-header").classList.add("header-bg-custom3")
+        $("nav-item-button3").classList = "nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill"
+        
+        break
+    }
+
+    case "pacil": {
+        $("navbar-header").classList.add("header-bg-custom4")
+        $("nav-item-button4").classList = "nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill"
+        
+        break
+    }
+}
+// nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill
