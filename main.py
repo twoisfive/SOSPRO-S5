56514420639b08532d607cce52dc250f82458ae0
ilman01 <ilmanzidni2006@gmail.com>
2025-09-22 22:28:14 +0700

    Added per page css

--- main.py
+++ main.py
@@ -14,25 +14,33 @@ scripts_files = sorted(f for f in os.listdir(scripts_folder) if f.endswith(".js"
 def home():
     main_section_folder = "templates/sections/main_sections"
     main_section_files = sorted(f for f in os.listdir(main_section_folder) if f.endswith(".html"))
-    return render_template("main.html", main_section_files=main_section_files, styles_files=styles_files, scripts_files=scripts_files)
+    section_style_folder = "templates/styles_per_page/main"
+    section_style_files = sorted(f for f in os.listdir(section_style_folder) if f.endswith(".css"))
+    return render_template("main.html", main_section_files=main_section_files, styles_files=styles_files, scripts_files=scripts_files, section_style_files=section_style_files)
 
 @app.route("/about-us")
 def about_us():
     section_folder = "templates/sections/about_us_sections"
     section_files = sorted(f for f in os.listdir(section_folder) if f.endswith(".html"))
-    return render_template("about_us.html", about_us_section_files=section_files, styles_files=styles_files, scripts_files=scripts_files)
+    section_style_folder = "templates/styles_per_page/about_us"
+    section_style_files = sorted(f for f in os.listdir(section_style_folder) if f.endswith(".css"))
+    return render_template("about_us.html", about_us_section_files=section_files, styles_files=styles_files, scripts_files=scripts_files, section_style_files=section_style_files)
 
 @app.route("/event")
 def event():
     section_folder = "templates/sections/event_sections"
     section_files = sorted(f for f in os.listdir(section_folder) if f.endswith(".html"))
-    return render_template("event.html", event_section_files=section_files, styles_files=styles_files, scripts_files=scripts_files)
+    section_style_folder = "templates/styles_per_page/event"
+    section_style_files = sorted(f for f in os.listdir(section_style_folder) if f.endswith(".css"))
+    return render_template("event.html", event_section_files=section_files, styles_files=styles_files, scripts_files=scripts_files, section_style_files=section_style_files)
 
 @app.route("/pacil")
 def pacil():
     section_folder = "templates/sections/pacil_sections"
     section_files = sorted(f for f in os.listdir(section_folder) if f.endswith(".html"))
-    return render_template("pacil.html", pacil_section_files=section_files, styles_files=styles_files, scripts_files=scripts_files)
+    section_style_folder = "templates/styles_per_page/pacil"
+    section_style_files = sorted(f for f in os.listdir(section_style_folder) if f.endswith(".css"))
+    return render_template("pacil.html", pacil_section_files=section_files, styles_files=styles_files, scripts_files=scripts_files, section_style_files=section_style_files)
 
 
 BASE_DIR = os.path.dirname(__file__)
