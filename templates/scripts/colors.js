2689f32f10c4244480b6243b98a55c169c530dc8
RedStone576 <redgaming576@gmail.com>
2025-09-24 01:36:40 +0700

    footer

--- templates/scripts/colors.js
+++ templates/scripts/colors.js
@@ -5,13 +5,15 @@ switch (window.location.pathname.split("/")[1] ?? "")
     case "": {
         $("navbar-header").classList.add("header-bg-custom1")
         $("nav-item-button1").classList = "nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill"
-
+        $("bottom-footer").classList.add("footer-bg-custom1")
+        
         break
     }
 
     case "about-us": {
         $("navbar-header").classList.add("header-bg-custom2")
         $("nav-item-button2").classList = "nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill"
+        $("bottom-footer").classList.add("footer-bg-custom2")
         
         break
     }
@@ -19,6 +21,7 @@ switch (window.location.pathname.split("/")[1] ?? "")
     case "event": {
         $("navbar-header").classList.add("header-bg-custom3")
         $("nav-item-button3").classList = "nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill"
+        $("bottom-footer").classList.add("footer-bg-custom2")
         
         break
     }
@@ -26,6 +29,7 @@ switch (window.location.pathname.split("/")[1] ?? "")
     case "pacil": {
         $("navbar-header").classList.add("header-bg-custom4")
         $("nav-item-button4").classList = "nav-link active btn btn-light text-dark fw-bold px-3 py-1 rounded-pill"
+        $("bottom-footer").classList.add("footer-bg-custom2")
         
         break
     }
